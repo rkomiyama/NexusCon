67f72ccd51f0019febb29726c68a6932d05cfe6f
rkomiyama <rkomiya@ncsu.edu>
2018-02-01 14:58:54 -0500

    Setup is mostly working

--- webpack.common.js
+++ webpack.common.js
@@ -3,21 +3,31 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: {
-    app: './src/index.js'
-  },
+  entry: [
+    'babel-polyfill',
+    'react-hot-loader/patch',
+    './client/index.js'
+  ],
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'MultiCon'
+      template: 'public/index.html'
     })
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
   },
   module: {
     rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader'
+        ],
+        exclude: /node_modules/
+      },
       {
         test: /\.css$/,
         use: [
--- webpack.dev.js
+++ webpack.dev.js
@@ -1,9 +1,16 @@
+const path = require('path');
+const webpack = require('webpack');
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist'
-  }
+    contentBase: path.resolve(__dirname, 'dist'),
+    hot: true
+  },
+  plugins: [
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ]
 });
