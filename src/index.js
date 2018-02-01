e11323a14559cfdd8bb3b9ece85ceba19d5d9514
rkomiyama <rkomiya@ncsu.edu>
2018-02-01 11:28:28 -0500

    Set up dev and prod webpack configs

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
 import './style.css';
 
+if (process.env.NODE_ENV !== 'production') {
+  console.log('Looks like we are in development mode!');
+}
+
 function component() {
   var element = document.createElement('div');
 
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hey', 'webpack'], ' ');
   element.classList.add('hello');
 
   return element;
