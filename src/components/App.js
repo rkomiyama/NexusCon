7d59553103bc1d3d1a20dcf3acc852d376ca9000
rkomiyama <rkomiya@ncsu.edu>
2018-02-02 17:10:21 -0500

    Adds more pages and changes path of Sass files

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import FrontPage from './pages/FrontPage';
 import AboutPage from './pages/AboutPage';
+import ActivitiesPage from './pages/ActivitiesPage';
+import GuestsPage from './pages/GuestsPage';
+import RegistrationPage from './pages/RegistrationPage';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Router>
+      <BrowserRouter>
         <div className="container">
           <Route exact path='/' component={FrontPage} />
           <Route path='/about' component={AboutPage} />
+          <Route path='/activities' component={ActivitiesPage} />
+          <Route path='/guests' component={GuestsPage} />
+          <Route path='/registration' component={RegistrationPage} />
         </div>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
