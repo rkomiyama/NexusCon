79473ccb6d98a6c9f4ca318e084a224dc5ea5720
rkomiyama <rkomiya@ncsu.edu>
2018-02-05 14:02:55 -0500

    Adds scroll lock for body when burger menu is showing

--- src/components/NavBurger.js
+++ src/components/NavBurger.js
@@ -1,75 +1,85 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NavBurger = () => (
-  <nav className="navburger">
-    <div id="menuToggle">
-      <div>
-        <input type="checkbox" />
-        <span></span>
-        <span></span>
-        <span></span>
-        <ul className="navburger__list u-full-width">
-          <li className="navburger__list-item">
-            <Link to="/">
-              Home
-            </Link>
-          </li>
-          <li className="navburger__list-item">
-            <Link to="/about">
-              About Us
-            </Link>
-          </li>
-          <li className="navburger__list-item">
-            <Link to="/activities">
-              Activities
-            </Link>
-          </li>
-          <li className="navburger__list-item">
-            <Link to="/guests">
-              Guests
-            </Link>
-          </li>
-          <li className="navburger__list-item">
-            <Link to="/registration">
-              Registration
-            </Link>
-          </li>
-          <li className="navburger__list-item">
-            <Link to="/activities">
-              Activities
-            </Link>
-          </li>
-          <li className="navburger__list-item">
-            <Link to="/guests">
-              Guests
-            </Link>
-          </li>
-          <li className="navburger__list-item">
-            <Link to="/registration">
-              Registration
-            </Link>
-          </li>
-          <li className="navburger__list-item">
-            <Link to="/activities">
-              Activities
-            </Link>
-          </li>
-          <li className="navburger__list-item">
-            <Link to="/guests">
-              Guests
-            </Link>
-          </li>
-          <li className="navburger__list-item">
-            <Link to="/registration">
-              Registration
-            </Link>
-          </li>
-        </ul>
+const NavBurger = () => {
+  const toggleBodyScrollLock = () => {
+    let checked = document.getElementById('menuCheckbox').checked;
+    if (checked) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }
+  return (
+    <nav className="navburger">
+      <div id="menuToggle">
+        <div>
+          <input id="menuCheckbox" type="checkbox" onClick={toggleBodyScrollLock} />
+          <span></span>
+          <span></span>
+          <span></span>
+          <ul className="navburger__list u-full-width">
+            <li className="navburger__list-item">
+              <Link to="/">
+                Home
+              </Link>
+            </li>
+            <li className="navburger__list-item">
+              <Link to="/about">
+                About Us
+              </Link>
+            </li>
+            <li className="navburger__list-item">
+              <Link to="/activities">
+                Activities
+              </Link>
+            </li>
+            <li className="navburger__list-item">
+              <Link to="/guests">
+                Guests
+              </Link>
+            </li>
+            <li className="navburger__list-item">
+              <Link to="/registration">
+                Registration
+              </Link>
+            </li>
+            <li className="navburger__list-item">
+              <Link to="/activities">
+                Activities
+              </Link>
+            </li>
+            <li className="navburger__list-item">
+              <Link to="/guests">
+                Guests
+              </Link>
+            </li>
+            <li className="navburger__list-item">
+              <Link to="/registration">
+                Registration
+              </Link>
+            </li>
+            <li className="navburger__list-item">
+              <Link to="/activities">
+                Activities
+              </Link>
+            </li>
+            <li className="navburger__list-item">
+              <Link to="/guests">
+                Guests
+              </Link>
+            </li>
+            <li className="navburger__list-item">
+              <Link to="/registration">
+                Registration
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="navburger__list-header"></div>
       </div>
-      <div className="navburger__list-header"></div>
-    </div>
-  </nav>
-);
+    </nav>
+  );
+};
 
 export default NavBurger;
