9b2e36775b4999a01fe989ac1491e6ca8a0de2bb
rkomiyama <rkomiya@ncsu.edu>
2018-02-06 11:38:57 -0500

    Adds background image and small layout fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import BgImage from '../assets/images/bg.jpg';
 
 const Header = () => (
-  <section className="header container">
-    <h1>MultiCon</h1>
+  <section className="header" style={{'backgroundImage': 'url(' + BgImage + ')'}}>
+    <div className="container">
+      <h1 className="header__heading">MultiCon</h1>
+    </div>
   </section>
 );
 
