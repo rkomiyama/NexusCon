a13e0106f16a906282287dcc2a1f259179e59f21
Ryunosuke Komiyama <Ryunosuke.Komiyama@ibm.com>
2018-03-24 13:11:03 -0400

    Adds overlay functionality

--- src/components/NavBurger.js
+++ src/components/NavBurger.js
@@ -13,6 +13,7 @@ export default class NavBurger extends React.Component {
   }
 
   componentDidMount() {
+    window.addEventListener('mousedown', this.pageClick);
     let parents = [];
 
     siteMap.routes.map((route, idx) => {
@@ -24,14 +25,23 @@ export default class NavBurger extends React.Component {
     this.setState({ parents });
   }
 
+  componentWillUnmount() {
+    window.removeEventListener('mousedown', this.pageClick);
+  }
+
   render () {
     return (
       <nav className="navburger">
         <div className="navburger__list-header"></div>
-        <div className={`navburger__icon ${this.state.checked ? 'open' : ''}`} onClick={() => this.toggleBodyScrollLock()}>
+        <div
+          className={`navburger__icon ${this.state.checked && 'open'}`}
+          onClick={() => this.toggleBodyScrollLock()}
+          ref={(node) => this.burgerIconRef = node}
+        >
           <span></span>
         </div>
-        <div className="navburger__list-wrapper u-full-width">
+        <div className={`${this.state.checked && 'navburger__overlay'}`}></div>
+        <div className="navburger__list-wrapper" ref={(node) => this.wrapperRef = node}>
           <ul className="navburger__list">
             {siteMap.routes.map((route, idx) => {
               return (
@@ -65,6 +75,15 @@ export default class NavBurger extends React.Component {
         </div>
       </nav>
     );
+  }
+
+  pageClick = (event) => {
+    // allows toggleBodyScrollLock when clicking outside of wrapper *and* burger icon
+    if (this.state.checked &&
+      ((this.wrapperRef && !this.wrapperRef.contains(event.target)) &&
+      (this.burgerIconRef && !this.burgerIconRef.contains(event.target)))) {
+      this.toggleBodyScrollLock();
+    }
   }
 
   toggleSubMenu = (parentIndex) => {
